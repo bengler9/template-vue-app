c5011cc9b47a1199a0172921988b2598eaf838ac
Brian Engler <bengler9@gmail.com>
2024-12-19 11:10:51 -0500

    Revert "simplfying for test"
    This reverts commit d3dc1980d7fc43cb7662a09034eb22c7599222af.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="home">
+    <div><h1>Good Day from CloudBot.AI!</h1></div>
+  </section>
+</template>
+
+<script></script>
+
+<style>
+@media (min-width: 1024px) {
+  .home {
+    min-height: 40vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="notfound">
+    <base-card>
+      <h1>Page Not Found</h1>
+      <p>
+        The requested page could not be found. Maybe take a look at our
+        <router-link to="/home">main screen</router-link> for more info.
+      </p>
+    </base-card>
+  </section>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .notfound {
+    min-height: 40vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
